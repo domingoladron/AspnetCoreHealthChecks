09729557e769ed4abce0c1f60f491a8ad5635dbc
Chris <chris@coactive.io>
2019-06-29 17:47:15 +1200

    Adding response writing to provide useful output to the health check caller

--- HealthChecks.SomeModelService/Startup.cs
+++ HealthChecks.SomeModelService/Startup.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
 using HealthChecks.Configuration;
@@ -103,7 +103,7 @@ namespace HealthChecks.SomeModelService
 
             app.UseHealthChecks("/health", new HealthCheckOptions()
             {
-                Predicate = _ => true
+                ResponseWriter = WriteHealthCheckResponse
             });
 
             //Use Identity and Access Control
@@ -111,6 +111,23 @@ namespace HealthChecks.SomeModelService
             
             //Use MVC / API
             app.UseMvc();
+        }
+
+        private static Task WriteHealthCheckResponse(HttpContext httpContext,
+            HealthReport result)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description),
+                        new JProperty("data", new JObject(pair.Value.Data.Select(
+                            p => new JProperty(p.Key, p.Value))))))))));
+            return httpContext.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
         }
 
         /// <summary>
