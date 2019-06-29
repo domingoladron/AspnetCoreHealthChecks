01b9672eab4fef2d29fe583903d8970eff7f205f
Chris <chris@coactive.io>
2019-06-29 16:08:36 +1200

    Nuke this, as boilerplate is not yet open source.

--- create-my-apis.cmd
+++
@@ -1,5 +0,0 @@
-dotnet new ptml-micro-ddd --model-name SomeModel --name HealthChecks.ExampleCrud.SomeModelService --base-namespace HealthChecks.ExampleCrud --force
-
-dotnet sln ./HealthChecks.ExampleCrud.sln add ./HealthChecks.ExampleCrud.SomeModelService/HealthChecks.ExampleCrud.SomeModelService.csproj
-
-dotnet add ./Tests/HealthChecks.ExampleCrud.Tests/HealthChecks.ExampleCrud.Tests.csproj reference ./HealthChecks.ExampleCrud.SomeModelService/HealthChecks.ExampleCrud.SomeModelService.csproj
